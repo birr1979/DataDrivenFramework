3633c9005eea242b8f194190721435135ae53373
birr_ <birr_@10.0.0.158>
2021-03-21 13:15:50 -0400

    Data Driven framework

---
+++ src/test/java/roughTrials/exam.java
@@ -0,0 +1,13 @@
+package roughTrials;
+
+import org.testng.annotations.Test;
+
+public class exam {
+
+
+	@Test
+	public void maven() {
+		System.out.println("this is a test project for jenkins.");
+	}
+
+}
---
+++ src/test/java/roughTrials/testiflaunches.java
@@ -0,0 +1,17 @@
+package roughTrials;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import listeners.TestListeners;
+@Listeners(TestListeners.class)
+
+public class testiflaunches {
+	
+	@Test
+	public void kate() {
+		
+		
+	}
+
+}
